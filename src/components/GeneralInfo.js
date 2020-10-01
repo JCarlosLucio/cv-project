2c1c5a5d36c56509ba55a5cabef95f5cb3f83815
jcLucio <c.lucio3@gmail.com>
2020-10-01 14:56:10 -0500

    Add GeneralInfo w/boilerplate

---
+++ src/components/GeneralInfo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class GeneralInfo extends Component {
+  render() {
+    return (
+      <div>
+        <h1>General Information</h1>
+      </div>
+    );
+  }
+}
+
+export default GeneralInfo;
