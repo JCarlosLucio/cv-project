9c2b2c86bd52d9f7753c8f372cbe73a7c2ebadaa
jcLucio <c.lucio3@gmail.com>
2020-10-01 16:22:18 -0500

    Add fullName

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
 export class GeneralInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullName: 'John',
+    };
+  }
+
   render() {
+    const { fullName } = this.state;
     return (
       <div>
         <h1>General Information</h1>
+        <p>{fullName}</p>
       </div>
     );
   }
