411b6e6a8d2e94d0ca424e7db85497190418f852
jcLucio <c.lucio3@gmail.com>
2020-10-01 17:35:33 -0500

    Add Education w/boilerplate

---
+++ src/components/EducationInfo.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export class EducationInfo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Education</h1>
+      </div>
+    );
+  }
+}
+
+export default EducationInfo;
