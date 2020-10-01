1da9f25ad06d14108e4134e8d82a4e333f701739
jcLucio <c.lucio3@gmail.com>
2020-10-01 14:50:29 -0500

    Refactor into class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <h1>CV PROJECT</h1>
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>CV PROJECT</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
