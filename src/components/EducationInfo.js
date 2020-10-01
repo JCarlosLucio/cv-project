b0847c24b6e6ce9f63fc8b98ac14efad1fcbc24b
jcLucio <c.lucio3@gmail.com>
2020-10-01 18:55:01 -0500

    Add school/title/date/isEditing state

--- src/components/EducationInfo.js
+++ src/components/EducationInfo.js
@@ -3,14 +3,24 @@ import React, { Component } from 'react';
 export class EducationInfo extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {};
+    this.state = {
+      school: 'Harvard',
+      title: 'Chemical Engineering',
+      date: '2019',
+      isEditing: false,
+    };
   }
 
   render() {
+    const { school, title, date, isEditing } = this.state;
     return (
       <div>
         <h1>Education</h1>
+        <div>
+          <p>{school}</p>
+          <p>{title}</p>
+          <p>{date}</p>
+        </div>
       </div>
     );
   }
