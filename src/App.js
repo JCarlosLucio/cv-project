8037fb3d9e3b88eda0fba3a0fd15e41aa977fbe4
jcLucio <c.lucio3@gmail.com>
2020-10-01 14:57:33 -0500

    Add GeneralInfo component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import GeneralInfo from './components/GeneralInfo';
 
 export class App extends Component {
   render() {
     return (
       <div>
         <h1>CV PROJECT</h1>
+        <GeneralInfo />
       </div>
     );
   }
