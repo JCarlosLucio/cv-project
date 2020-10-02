16407a56fd2ce43ded6422f9860380d73dea492c
jcLucio <c.lucio3@gmail.com>
2020-10-01 19:34:49 -0500

    Add WorkInfo w/boilerplate

---
+++ src/components/WorkInfo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class WorkInfo extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Work Experience</h1>
+      </div>
+    );
+  }
+}
+
+export default WorkInfo;
