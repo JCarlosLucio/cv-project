563da20f9b5b04d1f8ce9b7e6eb15c0992958e5a
jcLucio <c.lucio3@gmail.com>
2020-10-01 19:37:18 -0500

    Add company/position/tasks/dates/isEditing

--- src/components/WorkInfo.js
+++ src/components/WorkInfo.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 
 export class WorkInfo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      company: 'Papa Johns',
+      position: 'Potato Specialist',
+      tasks: 'Write emails, Rate potatoes',
+      startDate: '01/2017',
+      endDate: '03/2020',
+      isEditing: false,
+    };
+  }
+
   render() {
+    const {
+      company,
+      position,
+      tasks,
+      startDate,
+      endDate,
+      isEditing,
+    } = this.state;
     return (
       <div>
         <h1>Work Experience</h1>
+        <p>{company}</p>
+        <p>{position}</p>
+        <p>{tasks}</p>
+        <p>{startDate}</p>
+        <p>{endDate}</p>
       </div>
     );
   }
