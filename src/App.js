efe2c26f94a85aa018a42baa9c026935ab187170
jcLucio <c.lucio3@gmail.com>
2020-10-02 13:09:57 -0500

    Update styles

--- src/App.js
+++ src/App.js
@@ -11,7 +11,32 @@ import GeneralInfo from './components/GeneralInfo';
 import EducationInfo from './components/EducationInfo';
 import WorkInfo from './components/WorkInfo';
 
-const styles = (theme) => ({});
+const styles = (theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor: theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  info: {
+    backgroundColor: theme.palette.grey[900],
+    color: 'white',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+});
 
 export class App extends Component {
   render() {
