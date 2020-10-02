0d6e1df847e423ccb7feb2ad734b4e968c30f2f7
jcLucio <c.lucio3@gmail.com>
2020-10-01 19:35:15 -0500

    Add WorkInfo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import GeneralInfo from './components/GeneralInfo';
 import EducationInfo from './components/EducationInfo';
+import WorkInfo from './components/WorkInfo';
 
 export class App extends Component {
   render() {
@@ -9,6 +10,7 @@ export class App extends Component {
         <h1>CV PROJECT</h1>
         <GeneralInfo />
         <EducationInfo />
+        <WorkInfo />
       </div>
     );
   }
