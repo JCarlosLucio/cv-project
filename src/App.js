654d7d060c0d175cb3513e8da6c44fc3b57e5e17
jcLucio <c.lucio3@gmail.com>
2020-10-02 13:07:53 -0500

    Add material-ui component imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 import GeneralInfo from './components/GeneralInfo';
 import EducationInfo from './components/EducationInfo';
 import WorkInfo from './components/WorkInfo';
