593bc0877c5ee5ed24fba136038d5b68ea4fa573
jcLucio <c.lucio3@gmail.com>
2020-10-02 19:38:36 -0500

    Update elements w/material-ui components

--- src/components/WorkInfoForm.js
+++ src/components/WorkInfoForm.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = {
@@ -21,50 +25,78 @@ function WorkInfoForm({
   endDate,
   handleChange,
   handleSubmit,
+  classes,
 }) {
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Company:
-        <input
-          name="company"
-          value={company}
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <label>
-        Position:
-        <input
-          name="position"
-          value={position}
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <label>
-        Tasks:
-        <textarea name="tasks" value={tasks} onChange={handleChange} required />
-      </label>
-      <label>
-        Start Date:
-        <input
-          name="startDate"
-          value={startDate}
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <label>
-        End Date:
-        <input
-          name="endDate"
-          value={endDate}
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <button type="submit">Submit</button>
+    <form onSubmit={handleSubmit} className={classes.root}>
+      <Typography component="h1" variant="h5">
+        Edit Experience
+      </Typography>
+      <Grid container spacing={3} className={classes.container}>
+        <Grid item xs={6} sm={6}>
+          <TextField
+            required
+            id="company"
+            name="company"
+            label="Company"
+            value={company}
+            onChange={handleChange}
+            color="secondary"
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={3} sm={3}>
+          <TextField
+            required
+            id="startDate"
+            name="startDate"
+            label="Start Date"
+            value={startDate}
+            onChange={handleChange}
+            color="secondary"
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={3} sm={3}>
+          <TextField
+            required
+            id="endDate"
+            name="endDate"
+            label="End Date"
+            value={endDate}
+            onChange={handleChange}
+            color="secondary"
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} sm={12}>
+          <TextField
+            required
+            id="position"
+            name="position"
+            label="Position"
+            value={position}
+            onChange={handleChange}
+            color="secondary"
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} sm={12}>
+          <TextField
+            required
+            id="tasks"
+            name="tasks"
+            label="Tasks"
+            value={tasks}
+            onChange={handleChange}
+            color="secondary"
+            fullWidth
+          />
+        </Grid>
+        <Button type="submit" variant="contained" color="secondary">
+          Submit
+        </Button>
+      </Grid>
     </form>
   );
 }
