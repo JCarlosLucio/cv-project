7dd06044979d1c67c33c4ac190a4d55d50e4afaf
jcLucio <c.lucio3@gmail.com>
2020-10-04 11:53:28 -0500

    Updates

---
+++ precache-manifest.89028f10d135105aa4cc7d94d8ac1b6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c3f7333bca9dab3e253c1be56b1c701",
+    "url": "/cv-project/index.html"
+  },
+  {
+    "revision": "0d974efb82b685d32500",
+    "url": "/cv-project/static/css/main.cfbcba23.chunk.css"
+  },
+  {
+    "revision": "4c0fdd61b46ae7f275d7",
+    "url": "/cv-project/static/js/2.72b6019c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cv-project/static/js/2.72b6019c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d974efb82b685d32500",
+    "url": "/cv-project/static/js/main.534503b5.chunk.js"
+  },
+  {
+    "revision": "f1ebb3293288666c5950",
+    "url": "/cv-project/static/js/runtime-main.d91c792d.js"
+  }
+]);
