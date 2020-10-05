5bb2cd7a035c0a7d8e1d922606e4d7dd134117bd
jcLucio <c.lucio3@gmail.com>
2020-10-04 21:28:59 -0500

    Refactor to functional component w/custom hooks

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -1,77 +1,64 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import EditIcon from '@material-ui/icons/Edit';
 import { withStyles } from '@material-ui/core/styles';
+import useInputComplexState from '../hooks/useInputComplexState';
+import useToggleState from '../hooks/useToggleState';
 import GeneralInfoForm from './GeneralInfoForm';
 import styles from '../styles/GeneralInfoStyles';
 
-export class GeneralInfo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      fName: 'John',
-      lName: 'Smith',
-      email: 'john.smith@email.com',
-      phone: '(620) 454-1799',
-      isEditing: false,
-    };
-    this.toggleEdit = this.toggleEdit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+function GeneralInfo({ classes }) {
+  const [generalInfo, handleChange] = useInputComplexState({
+    fName: 'John',
+    lName: 'Smith',
+    email: 'john.smith@email.com',
+    phone: '(620) 454-1799',
+  });
 
-  handleChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
+  const [isEditing, toggleEdit] = useToggleState(false);
 
-  handleSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    this.toggleEdit();
-  }
+    toggleEdit();
+  };
 
-  toggleEdit() {
-    this.setState({ isEditing: !this.state.isEditing });
-  }
+  const { fName, lName, email, phone } = generalInfo;
 
-  render() {
-    const { fName, lName, email, phone, isEditing } = this.state;
-    const { classes } = this.props;
-    return (
-      <React.Fragment>
-        {isEditing ? (
-          <GeneralInfoForm
-            fName={fName}
-            lName={lName}
-            email={email}
-            phone={phone}
-            handleChange={this.handleChange}
-            handleSubmit={this.handleSubmit}
-          />
-        ) : (
-          <div className={classes.root}>
-            <Avatar className={classes.avatar} />
-            <Typography component="h1" variant="h4">
-              {fName} {lName}
-            </Typography>
-            <Typography variant="subtitle2">Email</Typography>
-            <Typography>{email}</Typography>
-            <Typography variant="subtitle2">Phone Number</Typography>
-            <Typography>{phone}</Typography>
-            <IconButton
-              color="secondary"
-              size="small"
-              onClick={this.toggleEdit}
-              className={classes.editBtn}
-            >
-              <EditIcon />
-            </IconButton>
-          </div>
-        )}
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      {isEditing ? (
+        <GeneralInfoForm
+          fName={fName}
+          lName={lName}
+          email={email}
+          phone={phone}
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+        />
+      ) : (
+        <div className={classes.root}>
+          <Avatar className={classes.avatar} />
+          <Typography component="h1" variant="h4">
+            {fName} {lName}
+          </Typography>
+          <Typography variant="subtitle2">Email</Typography>
+          <Typography>{email}</Typography>
+          <Typography variant="subtitle2">Phone Number</Typography>
+          <Typography>{phone}</Typography>
+          <IconButton
+            color="secondary"
+            size="small"
+            onClick={toggleEdit}
+            className={classes.editBtn}
+          >
+            <EditIcon />
+          </IconButton>
+        </div>
+      )}
+    </React.Fragment>
+  );
 }
 
 export default withStyles(styles, { withTheme: true })(GeneralInfo);
